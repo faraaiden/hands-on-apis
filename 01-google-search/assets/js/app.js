d70a57c5ac305e9a1afa063d983e70d8e30e39a1
faraaiden <faraaiden.contact@gmail.com>
2022-05-26 10:50:43 +0700

    Add a new page as a result page and fix styiling

--- 01-google-search/assets/js/app.js
+++ 01-google-search/assets/js/app.js
@@ -1,18 +1,53 @@
-const getAllProducts = async () => {
+const searchInput = document.querySelector('.search__input');
+const searchBtn = document.querySelector('.main-search__find');
+
+const getAllData = async () => {
   const options = {
     method: 'GET',
     headers: {
       'X-User-Agent': 'desktop',
       'X-Proxy-Location': 'US',
       'X-RapidAPI-Host': 'google-search3.p.rapidapi.com',
-      'X-RapidAPI-Key': /* YOUR API KEY */
-    }
+      'X-RapidAPI-Key': /* YOUR API KEY */,
+    },
   };
-  
-  fetch('https://google-search3.p.rapidapi.com/api/v1/search/q=codecademy', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+
+  try {
+    const response = await fetch(
+      `https://google-search3.p.rapidapi.com/api/v1/search/q=${searchInput.value}`,
+      options
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
-getAllProducts();
+/*
+============================
+Redirect to search-result.html
+============================
+*/
+const redirect = () => {
+  window.location.href = '/search-result.html';
+};
+
+/*
+======================
+Search all input value
+======================
+*/
+// Click button
+if (searchBtn) {
+  searchBtn.addEventListener('click', redirect);
+}
+
+// Or by pressing 'Enter' key
+searchInput.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    // Trigger the button element with a click
+    searchBtn.click();
+  }
+});
